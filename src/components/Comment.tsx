d1baa5f715ef73f0b48dce3f43e420b243ea1cd8
joaovcostas <jvc22102003@gmail.com>
2024-01-05 11:36:25 -0300

    avatar props interface

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,7 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
   return (
     <div className={styles.comment}>
-      <Avatar hasBorder={false} src="https://github.com/jvc22.png" />
+      <Avatar hasBorder={false} src="https://github.com/jvc22.png" alt="" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
